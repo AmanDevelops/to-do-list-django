a2473601a0ccccd5d6c4e81c7853c5147dac04da
AmanDevelops <amanpal2072004@gmail.com>
2025-02-08 03:04:51 +0530

    Add ToDoList model and implement task management functionality
    - Created ToDoList model with user, status, and task fields.
    - Registered ToDoList model in admin.
    - Updated views to handle task creation and status updates.
    - Enhanced index.html for task input and display.
    - Improved CSS for better styling and user experience.
    - Added plus icon for task submission button.

--- user/views.py
+++ user/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
+from .models import ToDoList
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'user/index.html')
+    if request.method == "POST":
+        try:
+            todo = ToDoList()
+            todo.status = False
+            todo.task = request.POST['todo']
+            todo.user = request.user.username
+            todo.save()
+        except KeyError:
+            try:
+                print("isChecked:", request.POST)
+                print("id:", request.POST.get("id"))
+                if "id" in request.POST:
+                    is_checked = request.POST.get("isChecked") == "True"
+                    print("Status:", is_checked)
+                    ToDoList.objects.filter(id=request.POST['id']).update(status=not is_checked)
+            except KeyError:
+                pass
+    data = ToDoList.objects.all()
+
+    return render(request, 'user/index.html', {"data": data})
